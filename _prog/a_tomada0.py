8d5d81421c84aaafdef1f993c819de9a4cb22c93
carlo <carlo@nce.ufrj.br>
2024-09-25 12:25:08 -0300

    🔰  add a_tomada0.py 24.09jb2 🛡️ (09/25)

--- _prog/a_tomada0.py
+++ _prog/a_tomada0.py
@@ -17,39 +17,107 @@ Texto(cena, "Olá, Vamos construir o Jardim Radical!").vai()
 # cena = Cena(relogio).vai()
 "Descomente a linha acima, removendo o *# * inicial e execute"
 _SET1_ = {
-    "script_name": "jo1", "script_div_id": "jo1",
+    "script_name": "to0", "script_div_id": "to0",
     "height": 200, "title": "Mais Cenas", "show_scenario": False, "console_height": 45
 }  # _SEC_
 # document["_so0_"].remove()
 # _VIT_
-from vitollino import Cena, Jogo
+from vitollino import Cena, Texto, Jogo, Elemento
+from cenario import Planilha, Paisagem
+ESCONDE = -4000
 Jogo(style=dict(height="500px", width="650px"), did="_jogo_").z()
-local = "/_ativo/jardim/{}.jpg"  # este {} vai ser substituído pelo format
-japones, relogio = local.format("japones"), local.format("relogio")
-mirante, tomjobim = local.format("mirante"), local.format("mirante")
-cena_japones = Cena(japones)
-cena_relogio = Cena(relogio, esquerda=cena_japones)
-cena_mirante = Cena(mirante, direita=cena_japones, esquerda=cena_relogio).vai()
-cena_relogio.direita = cena_mirante
-cena_japones.esquerda = cena_mirante
-cena_japones.direita = cena_relogio
-cena_tomjobim = Cena(tomjobim)  # edite esta linha para ter vizinhos
-"agora tente inserir a *cena_tomjobim* na ciranda"
+
+class UmaGarrafa:
+    def __init__(self):
+        imagem_da_praia = "_ativo/agentes/praia.jpeg"
+        mapa_praia = Planilha(imagem_da_praia, conta_lado=4.3)
+        p = Paisagem(mapa_praia.j[0]).vai()
+
+        self.garrafa = Elemento(img="/_ativo/agentes/carta_garrafa.png", w=30, h=10,
+             x=100, y=300, cena=p, o=0.5, vai=self.acha_garrafa)
+        self.garrafa.o = 0.3 #deixa a garrafa transparente
+        self.carta = Elemento(img="/_ativo/agentes/carta_velha.jpg", w=250, h=400,
+             x=ESCONDE, y=50, cena=p, texto="Mas o que está escrito aqui?", foi=self.aparece_kayke)
+        self.kayke = Elemento(img="/_ativo/agentes/kayke.png", w=250, h=400,
+             x=ESCONDE, y=50, cena=p, texto="Deixa que eu vou decifrar")
+
+    def acha_garrafa(self, *_):
+        self.carta.x = 100 # tras a carta para a cena
+        self.garrafa.vai = self.nada_faz
+
+    def nada_faz(self, *_):
+        pass
+
+    def aparece_kayke(self, *_):
+        # escreva aqui algo semelhante a acha_garrafa
+        self.kayke.x = 100
+        pass
+
+UmaGarrafa()
 _SET2_ = {
-    "script_name": "jo2", "script_div_id": "jo2",
+    "script_name": "to1", "script_div_id": "to1",
     "height": 200, "title": "Criando Salas", "show_scenario": False, "console_height": 45
 }  # _SEC_
-# document["_so0_"].remove()
-# _VIT_
-from vitollino import Sala, Elemento, Texto, Jogo
+from vitollino import Cena, Texto, Jogo, Elemento, INVENTARIO as INV
+from cenario import Planilha, Paisagem, Paisagens
+from random import shuffle
 Jogo(style=dict(height="500px", width="650px"), did="_jogo_").z()
-cenas = "japones relogio mirante tomjobim".split()
-jardim = Sala(*[f"/_ativo/jardim/{cena}.jpg" for cena in cenas])
-cena = jardim.sul.vai()
-mais_cenas = "orquidario portal lago palmeiras".split()
-mais_jardim = Sala()  # complete aqui vendo o exemplo acima
-jardim.norte.meio = mais_jardim.norte
-#ligue agora o sul do mais_jardim no sul do jardim
+ESCONDIDO = -4000
+
+
+class UmaGarrafa:
+    def __init__(self):
+        imagem_da_praia = "_ativo/agentes/praia.jpeg"
+        mapa_praia = Planilha(imagem_da_praia, conta_lado=4.3)
+        pg = Paisagens(mapa_praia.j)
+        p = pg.norte
+        locais = [pg.norte, pg.leste, pg.sul, pg.oeste]
+        shuffle(locais)
+        alturas = [x for x in range(260, 460, 25)]
+        shuffle(alturas)
+        distancias = [x for x in range(100, 560, 50)]
+        shuffle(distancias)
+        p.vai()
+        local_carta = locais.pop()
+
+        self.garrafa = Elemento(img="/_ativo/agentes/carta_garrafa.png", w=30, h=10,
+             x=100, y=300, cena=p, o=0.5, vai=self.acha_garrafa)
+        self.garrafa.o = 0.3 #deixa a garrafa transparente
+        self.carta = Elemento(img="/_ativo/agentes/carta_velha.jpg", w=250, h=400,
+             x=ESCONDIDO, y=50, cena=p, texto="Mas o que está escrito aqui?", foi=self.aparece_kayke)
+        self.kayke = Elemento(img="/_ativo/agentes/kayke.png", w=250, h=400,
+             x=ESCONDIDO, y=50, cena=p, texto="Deixa que eu vou decifrar")
+
+        self.bau = Elemento(img="/_ativo/agentes/bau.png", w=50, h=40,
+                       x=ESCONDIDO, y=250, cena=locais.pop(),
+                       texto="O que tem dentro do baú? Precisa de chave!", foi=self.precisa_chave)
+        self.bau.o = 0.4
+        self.chave = Elemento(img="/_ativo/agentes/chave.png", w=50, h=40,
+                         x=ESCONDIDO, y=250, cena=locais.pop(), texto="Achamos a chave!", foi=self.tem_chave)
+        self.chave.o = 0.4
+
+    def tem_bau(self, *_):
+        pass # troque aqui para fazer um baú aparecer
+
+    def acha_garrafa(self, *_):
+        self.carta.x = 100 # tras a carta para a cena
+        self.garrafa.vai = self.nada_faz
+
+    def nada_faz(self, *_):
+        pass
+
+    def aparece_kayke(self, *_):
+        # escreva aqui algo semelhante a acha_garrafa
+        self.kayke.x = 100
+        pass
+
+    def precisa_chave(self):
+        pass # faça a chave aparecer
+
+    def tem_chave(self):
+        pass # agora faz alguma coisa quando clica no baú
+
+UmaGarrafa()
 _SET3_ = {
     "script_name": "jo3", "script_div_id": "jo3",
     "height": 200, "title": "Povoando o Jardim", "show_scenario": False, "console_height": 45
